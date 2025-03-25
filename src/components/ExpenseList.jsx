f121a3d8e91f17a1ce7abcc4514f15ace63deca8
KyoungMIn YOO <91euriel@gmail.com>
2025-03-25 22:58:33 +0900

    add: ExpenseList.jsx 주석 수정

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 function ExpenseList({ expenses }) {
   const navigate = useNavigate();
 
+  // toLocaleString 숫자 단위별로 , 기입 메소드
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-2">
       {expenses.length === 0 ? (
